dfdabfbb09434ed8de5146d24c53b2b69a8fc661
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-07-06 19:54:58 -0400

    Delete route completed

--- server.js
+++ server.js
@@ -20,12 +20,7 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 });
 
-// Wildcard Route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
-});
-
-// API Routes
+// API Get Route
 app.get('/api/notes', (req, res) => {
   // Using fs module, read the db.json file and return all saved notes as JSON
   fs.readFile('./db/db.json', (err, data) => {
@@ -38,8 +33,11 @@ app.get('/api/notes', (req, res) => {
   });
 });
 
+// Post Route
 app.post('/api/notes', (req, res) => {
+  // Grab new note on request body
   const newNote = req.body;
+  // Implements the uniqid module to give each note a unique id
   newNote.id = uniqid();
 
   fs.readFile('./db/db.json', (err, data) => {
@@ -47,9 +45,11 @@ app.post('/api/notes', (req, res) => {
       console.error(err);
       return res.status(500).send('Error reading notes');
     }
+    // Push the new note to the array
     const notes = JSON.parse(data);
     notes.push(newNote);
 
+    // Update JSON file with new note
     fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
       if (err) {
         console.error(err);
@@ -59,6 +59,38 @@ app.post('/api/notes', (req, res) => {
       res.json(newNote);
     });
   });
+});
+
+// DELETE Route
+app.delete('/api/notes/:id', (req, res) => {
+  // Receives a query parameter containing the id of a note to delete
+  const noteId = req.params.id;
+
+  fs.readFile('./db/db.json', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Error reading notes');
+    }
+
+    // Reads all notes and filters for note with given id
+    let notes = JSON.parse(data);
+    const updatedNotes = notes.filter(note => note.id !== noteId);
+
+    // Rewrites the notes to db.json
+    fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Error deleting note');
+      }
+
+      res.json({ message: 'Note deleted successfully' });
+    });
+  });
+});
+
+// Wildcard Route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 });
 
 app.listen(PORT, () =>
